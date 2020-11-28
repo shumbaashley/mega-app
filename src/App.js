d22187e11b5a10c68fa55d11897f027a07472c10
shumbaashley <ashleytshumba@gmail.com>
2020-11-28 13:54:04 +0200

    added material ui components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import Dashboard from './components/dashboard/Dashboard.js'
+import Checkout from './components/checkout/Checkout.js'
+import Blog from './components/blog/Blog.js'
+import Album from './components/album/Album.js'
+import Pricing from './components/pricing/Pricing.js'
+import SignIn from './components/sign-in/SignIn.js'
+import SignInSide from './components/sign-in-side/SignInSide.js'
+import SignUp from './components/sign-up/SignUp.js'
+
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>
-      <h1>Client App</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/dashboard" component={Dashboard}/>
+        <Route exact path="/checkout" component={Checkout}/>
+        <Route exact path="/blog" component={Blog}/>
+        <Route exact path="/album" component={Album}/>
+        <Route exact path="/pricing" component={Pricing}/>
+        <Route exact path="/album" component={Album}/>
+        <Route exact path="/sign-in" component={SignIn}/>
+        <Route exact path="/sign-in-side" component={SignInSide}/>
+        <Route exact path="/sign-up" component={SignUp}/>
+      </Switch>
+    </Router>
   );
 }
 
